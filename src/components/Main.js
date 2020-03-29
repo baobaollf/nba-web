1d12e966e741c063b07a09ef8fa5244e42c91d5c
baobaollf <55776365+baobaollf@users.noreply.github.com>
2020-03-29 17:02:07 -0400

    with slider

---
+++ src/components/Main.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import nba from '../nba_client';
+import Profile from './Profile';
+import DataViewContainer from "./DataViewContainer";
+
+class Main extends Component {
+    constructor() {
+        super();
+        this.state = {
+            playerInfo: {},
+            playerId: 201939,
+        }
+    }
+    componentDidMount() {
+        window.nba = nba;
+        nba.stats.playerInfo({ PlayerID: nba.findPlayer('Stephen Curry').playerId})
+            .then((info) => {
+                console.log(info);
+                const playInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+                console.log(playInfo);
+                this.setState({ playerInfo: playInfo });
+            })
+    }
+
+
+    render() {
+        return (
+            <div className="main">
+                <Profile playerInfo={ this.state.playerInfo}/>
+                <DataViewContainer playerId={ this.state.playerId}/>
+            </div>
+        );
+    }
+}
+
+export default Main;
